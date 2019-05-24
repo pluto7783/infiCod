e7d9f49ec5da447210c401523a6de547f7c087e0
unknown <karolus@naver.com>
2019-05-24 14:49:44 +0900

    chrome extensions

---
+++ infiCod/WebContent/WEB-INF/jsp/CE/background.js
@@ -0,0 +1,7 @@
+chrome.browserAction.onClicked.addListener(
+	function (tab) {
+//		chrome.tabs.executeScript(null, { file: "jquery-3.4.1.min.js" }, function() {
+//		    chrome.tabs.executeScript(null, { file: "inject.js" });
+//		});
+	}
+)
---
+++ infiCod/WebContent/WEB-INF/jsp/CE/js/inject.js
@@ -0,0 +1,27 @@
+function onLoadFuc(){
+
+	/*$(function(){
+		if($("#topDiv")[0]){
+			$("#topDiv").remove();
+			return;
+		}
+		var topD = $("<div>", {id : "topDiv"}).css({"width":"100px",
+													"height":"100px",
+													"backgroundColor":"white",
+													"position":"fixed",
+													"zIndex":"999999",
+													"bottom":"100px",
+													"right":"100px",
+													"border":"1px solid #64646433"});
+		
+		$("body").append(topD);
+		
+		console.log("===== 테스트 009 =====")
+	});*/
+	
+	$("img").remove();
+
+	console.log("===== 테스트 12 =====")
+}
+
+window.onload = onLoadFuc;
